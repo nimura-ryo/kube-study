bf82a8c117087b0ba0dca40df4d14b3dd99a4006
新村 良 <nimura.ryo@wing.sysrdc.com>
2020-04-26 05:03:40 +0900

    AAA

--- api/index.js
+++ api/index.js
@@ -2,5 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/api', (req, res) => res.send('Hello API!'))
+app.get('/api/hello', (req, res) => res.send('Hello hello!'))
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
